0a84d78b6d6d80fe5176d39e4627f2737de41eb1
moniradiu <moniradiu@gmail.com>
2021-03-05 03:07:35 +0600

    error code list with image

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: unused_import
 import 'package:MyApp3/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp3());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -28,3 +29,5 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
+class MyApp {}
